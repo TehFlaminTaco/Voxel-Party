2fffdc5509c7a71c3c7c2c104e0e0a170c73d659
DrakeFruit <94070205+DrakeFruit@users.noreply.github.com>
2025-06-17 23:11:21 -0700

    remove log, hotbar styling

--- Code/theoretical/ItemStack.cs
+++ Code/theoretical/ItemStack.cs
@@ -18,7 +18,6 @@ public class ItemStack
         }
         set
         {
-            Log.Info( value );
             Item = ItemRegistry.GetItem( value ); // Get the item from the registry using the provided ID
         }
     }
